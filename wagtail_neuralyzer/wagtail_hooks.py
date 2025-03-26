f41eed3ba37415e6a1f663b2312e49e8cf0bf552
Samuël WEBER <s.weber@webu.coop>
2025-03-26 12:26:12 +0100

    feat: move translations 'neuralize' to 'anonymize'

--- wagtail_neuralyzer/wagtail_hooks.py
+++ wagtail_neuralyzer/wagtail_hooks.py
@@ -5,7 +5,7 @@ from wagtail import hooks
 
 @hooks.register("register_log_actions")
 def additional_log_actions(actions):
-    actions.register_action("wagtail_neuralyzer.neuralyze", _("Neuralyze"), _("Was neuralyze"))
+    actions.register_action("wagtail_neuralyzer.neuralyze", _("Neuralyze"), _("Was anonymize"))
 
 
 # @hooks.register("register_bulk_action")
