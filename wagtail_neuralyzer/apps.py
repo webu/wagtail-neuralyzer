9d06c826159c6e8c69aee1a0d6db5fb6b3af307f
Samuël WEBER <s.weber@webu.coop>
2025-03-28 12:18:55 +0100

    feat: add apps.py

---
+++ wagtail_neuralyzer/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailNeuralyzerConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wagtail_neuralyzer"
+    label = "wagtail_neuralyzer"
