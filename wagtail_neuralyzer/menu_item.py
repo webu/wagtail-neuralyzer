ef1915667a779be7afe686871478b3041fc86921
Samuël WEBER <s.weber@webu.coop>
2025-03-28 12:44:33 +0100

    feat(i18n): add label i18n

--- wagtail_neuralyzer/menu_item.py
+++ wagtail_neuralyzer/menu_item.py
@@ -1,12 +1,13 @@
 from django.contrib.admin.utils import quote
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from wagtail.snippets.action_menu import ActionMenuItem
 
 
 class NeuralyzeMenuItem(ActionMenuItem):
     name = "anonymize"
-    label = "Anonymize"
+    label = _("Anonymize")
     icon_name = "no-view"
 
     def get_url(self, context):
